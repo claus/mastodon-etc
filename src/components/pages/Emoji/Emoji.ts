2a1af56bd993db15591bc6a4f182cf7b6d51ac33
Claus Wahlers <claus@codeazur.com.br>
2025-12-28 09:11:09 -0300

    Emoji page: Preserve caption label

--- src/components/pages/Emoji/Emoji.ts
+++ src/components/pages/Emoji/Emoji.ts
@@ -14,18 +14,20 @@ export default class Emoji extends HTMLElement {
             navigator.clipboard
                 .writeText(`:${code}:`)
                 .then(() => {
-                    caption.textContent = 'copiado ✓';
-                    caption.classList.add('copied');
                     const prevTimeoutId = caption.dataset.timeoutId;
                     if (prevTimeoutId) {
                         window.clearTimeout(Number(prevTimeoutId));
                     }
                     const timeoutId = window.setTimeout(() => {
-                        caption.textContent = `:${code}:`;
+                        caption.textContent = caption.dataset.captionText ?? '';
                         caption.classList.remove('copied');
                         delete caption.dataset.timeoutId;
+                        delete caption.dataset.captionText;
                     }, 1000);
                     caption.dataset.timeoutId = timeoutId.toString();
+                    caption.dataset.captionText ??= caption.textContent;
+                    caption.classList.add('copied');
+                    caption.textContent = 'copiado ✓';
                 })
                 .catch(err => {
                     console.error('Failed to copy emoji:', err);
